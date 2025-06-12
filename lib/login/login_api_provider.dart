0f3a0e675619a0967b0b7e73698945b939e9eb8d
dw0124 <doowone8650@naver.com>
2025-06-12 17:56:04 +0900

    로그인을 위한 api Provider

---
+++ lib/login/login_api_provider.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class LoginApiProvider {
+  LoginApiProvider({http.Client? httpClient})
+      : _httpClient = httpClient ?? http.Client();
+
+  final http.Client _httpClient;
+
+  Future<Map<String, dynamic>> requestLogin({required String id, required String pwd}) async {
+    final String baseURL = dotenv.env['BASE_URL']!;
+
+    final url = Uri.https(
+      baseURL,
+      "/api/auth/sign-in",
+    );
+
+    final body = jsonEncode({
+      "studentNumber": id,
+      "password": pwd
+    });
+
+    var response = await _httpClient.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: body,
+    );
+
+    if (response.statusCode == 200) {
+      final jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
+
+      print(jsonResponse);
+
+      return jsonResponse;
+    } else {
+      throw Exception('Failed to Login: ${response.statusCode}');
+    }
+  }
+
+  void close() { _httpClient.close(); }
+}
