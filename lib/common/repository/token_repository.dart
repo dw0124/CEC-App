82108569f8505dee63b13af2739d6d7e7b628083
dw0124 <doowone8650@naver.com>
2025-06-13 17:43:48 +0900

    TokenRepository - 토큰 관리하는 repository

---
+++ lib/common/repository/token_repository.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class TokenRepository {
+  static final TokenRepository _instance = TokenRepository._internal();
+
+  TokenRepository._internal();
+
+  factory TokenRepository() => _instance;
+
+  final _storage = const FlutterSecureStorage();
+
+  Future<void> saveAccessToken(String token) async {
+    await _storage.write(key: 'access_token', value: token);
+  }
+
+  Future<String?> getAccessToken() async {
+    return await _storage.read(key: 'access_token');
+  }
+
+  Future<void> deleteAccessToken() async {
+    await _storage.delete(key: 'access_token');
+  }
+}
