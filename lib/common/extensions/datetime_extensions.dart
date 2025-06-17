d0b6654689d41e89e2b82f904a9be2188d6cefe8
dw0124 <doowone8650@naver.com>
2025-06-17 18:26:49 +0900

    DatetimeExtensions - timeAgo 추가

---
+++ lib/common/extensions/datetime_extensions.dart
@@ -0,0 +1,18 @@
+extension DatetimeExtensions on DateTime {
+  String timeAgo() {
+    final now = DateTime.now().toLocal();
+    final difference = now.difference(this);
+
+    if (difference.inMinutes < 1) {
+      return '방금 전';
+    } else if (difference.inMinutes < 60) {
+      return '${difference.inMinutes}분 전';
+    } else if (difference.inHours < 24) {
+      return '${difference.inHours}시간 전';
+    } else if (difference.inDays < 7) {
+      return '${difference.inDays}일 전';
+    } else {
+      return '$year.${month.toString().padLeft(2, '0')}.${day.toString().padLeft(2, '0')}';
+    }
+  }
+}
