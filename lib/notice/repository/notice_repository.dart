e6d8baee55b600cf4b1e198dc02800a379d34567
dw0124 <doowone8650@naver.com>
2025-06-19 17:54:48 +0900

    fetchNotices - Futurue<NoticeResponse> 리턴

--- lib/notice/repository/notice_repository.dart
+++ lib/notice/repository/notice_repository.dart
@@ -1,3 +1,4 @@
+import 'package:induk/notice/model/notice_response.dart';
 import 'package:induk/notice/provider/notice_api_provider.dart';
 
 class NoticeRepository {
@@ -6,15 +7,13 @@ class NoticeRepository {
 
   final NoticeApiProvider _noticeApiProvider;
 
-  Future<bool> fetchNotices({required int page}) async {
+  Future<NoticeResponse> fetchNotices({required int page}) async {
     try {
       final json = await _noticeApiProvider.fetchNotices(page: page);
-
-      print(json);
-
-      return true;
+      final noticeResponse = NoticeResponse.fromJson(json);
+      return noticeResponse;
     } catch (error) {
-      throw Exception("공지사항 불러오기 실패: $error");
+      throw Exception("NoticeRepository fetchNotices - 공지사항 불러오기 실패: $error");
     }
   }
 
