5bfb2324ca121e4ae677fb127de1a68ac680657b
dw0124 <doowone8650@naver.com>
2025-06-22 23:38:52 +0900

    NoticeList - GestureDetector/onTap 콜백 추가

--- lib/notice/view/widget/notice_list_item.dart
+++ lib/notice/view/widget/notice_list_item.dart
@@ -4,45 +4,50 @@ import 'package:induk/common/models/notice.dart';
 
 class NoticeListItem extends StatelessWidget {
   final Notice notice;
+  final VoidCallback onTap;
 
-  const NoticeListItem({super.key, required this.notice});
+  const NoticeListItem({super.key, required this.notice, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 80,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            notice.title,
-            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
-          ),
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: onTap,
+      child: SizedBox(
+        height: 80,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              notice.title,
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+            ),
 
-          Text(
-            notice.content,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16),
-          ),
+            Text(
+              notice.content,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16),
+            ),
 
 
-          Row(
-            children: [
-              Text(
-                "${notice.author.nickname}[${notice.author.role}] ・ 조회수 ${notice.view}",
-                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
-              ),
+            Row(
+              children: [
+                Text(
+                  "${notice.author.nickname}[${notice.author.role}] ・ 조회수 ${notice.view}",
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
+                ),
 
-              Spacer(),
+                Spacer(),
 
-              Text(
-                "${notice.createdAt.timeAgo()}",
-                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
-              ),
-            ],
-          ),
-        ],
+                Text(
+                  "${notice.createdAt.timeAgo()}",
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
