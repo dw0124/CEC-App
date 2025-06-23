3c101c5a55d7d38a8b7fc77450c5781d317e6312
dw0124 <doowone8650@naver.com>
2025-06-23 16:51:50 +0900

    StatelessWidget -> StatefulWidget으로 변경

--- lib/notice/view/notice_page.dart
+++ lib/notice/view/notice_page.dart
@@ -4,10 +4,23 @@ import 'package:induk/common/models/notice.dart';
 import 'package:induk/common/widgets/custom_search_bar.dart';
 import 'package:induk/notice/bloc/notice_bloc.dart';
 import 'package:induk/notice/bloc/notice_state.dart';
+import 'package:induk/notice/view/notice_detail_page.dart';
 import 'package:induk/notice/view/widget/notice_list_item.dart';
 
-class NoticePage extends StatelessWidget {
+class NoticePage extends StatefulWidget {
   const NoticePage({super.key});
+
+  @override
+  State<NoticePage> createState() => _NoticePageState();
+}
+
+class _NoticePageState extends State<NoticePage> {
+  @override
+  void initState() {
+    super.initState();
+
+    context.read<NoticeBloc>().add(NoticeFetchEvent());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,9 +42,11 @@ class NoticePage extends StatelessWidget {
                     padding: const EdgeInsets.only(top: 1),
                     child: DropdownSearchBar(
                         categories: ["전체", "제목", "내용"],
-                        onSearch: (category, query) {
+                        onSearch: (category, keyword) {
                           context.read<NoticeBloc>().add(
-                              NoticeFetchEvent()
+                              NoticeFetchEvent(
+                                keyword: keyword
+                              )
                           );
                         }
                     ),
@@ -55,6 +70,15 @@ class NoticePage extends StatelessWidget {
                               return NoticeListItem(
                                 key: ValueKey(noticeList[index].id),
                                 notice: noticeList[index],
+                                onTap: () {
+                                  final noticeDetailPage = NoticeDetailPage(index: index);
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => noticeDetailPage,
+                                      )
+                                  );
+                                }
                               );
                             },
                             separatorBuilder: (BuildContext context, int index) => Divider(
