7b9b9c9ae5c7f9051966a49a672d9cf3c42a349a
dw0124 <doowone8650@naver.com>
2025-07-03 18:08:57 +0900

    CartState init

---
+++ lib/cart/bloc/cart_state.dart
@@ -0,0 +1,40 @@
+import 'package:equatable/equatable.dart';
+import 'package:induk/cart/model/cart_item.dart';
+
+enum CartStatus { initial, loading, success, failure }
+
+extension NoticeStatusX on CartStatus {
+  bool get isInitial => this == CartStatus.initial;
+  bool get isLoading => this == CartStatus.loading;
+  bool get isSuccess => this == CartStatus.success;
+  bool get isFailure => this == CartStatus.failure;
+}
+
+class CartState extends Equatable {
+
+  final CartStatus cartStatus;
+  final List<CartItem> cartItems;
+  final Set<String> selectedItemsId;
+
+  const CartState({
+    this.cartStatus = CartStatus.initial,
+    this.cartItems = const [],
+    this.selectedItemsId = const {},
+  });
+
+  CartState copyWith({
+    CartStatus? cartStatus,
+    List<CartItem>? cartItems,
+    Set<String>? selectedItemsId,
+  }) {
+    return CartState(
+      cartStatus: cartStatus ?? this.cartStatus,
+      cartItems: cartItems ?? this.cartItems,
+      selectedItemsId: selectedItemsId ?? this.selectedItemsId,
+    );
+  }
+
+  @override
+  List<Object?> get props => [cartStatus, cartItems, selectedItemsId];
+}
+
