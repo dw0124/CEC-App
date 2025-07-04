8e2dacca48c06094b7487754eff0f1aaec36d651
dw0124 <doowone8650@naver.com>
2025-07-04 22:10:20 +0900

    CartEvent 수정

--- lib/cart/bloc/cart_event.dart
+++ lib/cart/bloc/cart_event.dart
@@ -4,21 +4,25 @@ sealed class CartEvent {}
 final class CartFetch extends CartEvent {}
 
 /// 상단 버튼 - 장바구니에서 장비 전체 선택
-final class CartSelectAll extends CartEvent {}
+final class CartSelectAll extends CartEvent {
+  final bool isSelected;
+
+  CartSelectAll({required this.isSelected});
+}
 
 /// 상단 버튼 - 장바구니에서 선택된 장비 삭제
 final class CartDeleteSelectedItems extends CartEvent {}
 
 /// 장바구니에서 장비 선택
 final class CartSelectItem extends CartEvent {
-  final String selectedItemId;
+  final int selectedItemId;
 
   CartSelectItem({required this.selectedItemId});
 }
 
 /// 장바구니에서 단일 삭제
 final class CartDeleteItem extends CartEvent {
-  final String selectedItemId;
+  final int selectedItemId;
 
   CartDeleteItem({required this.selectedItemId});
 }
