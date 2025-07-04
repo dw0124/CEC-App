3dc67102cc424547708ccfeba96357ea9dd05e75
dw0124 <doowone8650@naver.com>
2025-07-04 22:16:16 +0900

    CartPage - CartBloc 연결

--- lib/cart/cart_page.dart
+++ lib/cart/cart_page.dart
@@ -1,35 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:induk/cart/bloc/cart_bloc.dart';
+import 'package:induk/cart/bloc/cart_event.dart';
+import 'package:induk/cart/bloc/cart_state.dart';
 import 'package:induk/cart/model/cart_item.dart';
 import 'package:induk/cart/view/widget/cart_list_item.dart';
-import 'package:induk/common/models/equipment.dart';
 
 class CartPage extends StatefulWidget {
-  List<CartItem> cartList = [
-    CartItem(equipment: Equipment.empty, startDate: DateTime.now(), endDate: DateTime.now()),
-    CartItem(equipment: Equipment.empty, startDate: DateTime.now(), endDate: DateTime.now()),
-    CartItem(equipment: Equipment.empty, startDate: DateTime.now(), endDate: DateTime.now()),
-    CartItem(equipment: Equipment.empty, startDate: DateTime.now(), endDate: DateTime.now()),
-    CartItem(equipment: Equipment.empty, startDate: DateTime.now(), endDate: DateTime.now()),
-    CartItem(equipment: Equipment.empty, startDate: DateTime.now(), endDate: DateTime.now())
-  ];
-
-  CartPage({super.key});
+  const CartPage({super.key});
 
   @override
   State<CartPage> createState() => _CartPageState();
 }
 
 class _CartPageState extends State<CartPage> {
-
   @override
   void initState() {
-
     super.initState();
   }
 
   @override
   void dispose() {
-
     super.dispose();
   }
 
@@ -40,10 +31,7 @@ class _CartPageState extends State<CartPage> {
         scrolledUnderElevation: 0,
         elevation: 0,
         shape: Border(
-          bottom: BorderSide(
-            color: Colors.grey.shade300,
-            width: 1,
-          ),
+          bottom: BorderSide(color: Colors.grey.shade300, width: 1),
         ),
         title: Text(
           '장바구니',
@@ -53,10 +41,7 @@ class _CartPageState extends State<CartPage> {
           preferredSize: Size.fromHeight(56), // 버튼 높이에 맞춰 조정
           child: Column(
             children: [
-              Divider(
-                height: 1,
-                color: Colors.grey.shade300,
-              ),
+              Divider(height: 1, color: Colors.grey.shade300),
 
               Padding(
                 padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
@@ -66,69 +51,88 @@ class _CartPageState extends State<CartPage> {
                     // 전체 선택 버튼
                     Row(
                       children: [
-                        Checkbox(
-                            value: false,
+                        BlocSelector<CartBloc, CartState, bool>(
+                        selector: (state) {
+                          return state.selectedItemsId.isNotEmpty && state.selectedItemsId.length == state.cartItems.length;
+                        },
+                        builder: (context, isSelected) {
+                          return Checkbox(
+                            value: isSelected,
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(2.0),
                             ),
                             side: WidgetStateBorderSide.resolveWith(
-                                  (states) => BorderSide(width: 1.2, color: Colors.grey),
+                                  (states) =>
+                                  BorderSide(width: 1.2, color: Colors.grey),
                             ),
                             onChanged: (value) {
-                        
-                            }
-                        ),
-                        
+                              if (value != null) {
+                                context.read<CartBloc>().add(CartSelectAll(isSelected: value));
+                              }
+                            },
+                          );
+                        }),
+
                         Text(
-                            '전체 선택',
+                          '전체 선택',
                           style: TextStyle(fontWeight: FontWeight.w600),
-                        )
+                        ),
                       ],
                     ),
 
                     // 선택 삭제 버튼
                     TextButton(
-                        onPressed: () {},
-                        child: Text(
-                            '선택삭제',
-                          style: TextStyle(
-                            color: Colors.grey.shade600
-                          ),
-                        ),
-                    )
+                      onPressed: () => context.read<CartBloc>().add(CartDeleteSelectedItems()),
+                      child: Text(
+                        '선택삭제',
+                        style: TextStyle(color: Colors.grey.shade600),
+                      ),
+                    ),
                   ],
                 ),
-              )
+              ),
             ],
-          )
+          ),
         ),
       ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-          child: ListView.separated(
-            itemCount: widget.cartList.length,
-            scrollDirection: Axis.vertical,
-            itemBuilder: (context, index) {
-              return CartListItem(
-                key: ValueKey( widget.cartList[index].equipment.id),
-                cartItem:  widget.cartList[index],
-                isSelected: true,
-                onSelectChanged: (value) {
-                  print("$index: $value");
+          child: BlocSelector<CartBloc, CartState, List<CartItem>>(
+            selector: (state) => state.cartItems,
+            builder: (context, cartItems) {
+              return ListView.separated(
+                itemCount: cartItems.length,
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) {
+                  return CartListItem(
+                    key: ValueKey(cartItems[index].equipment.id),
+                    cartItem: cartItems[index],
+                    isSelected: true,
+                    onSelectChanged: (value) {
+                      final selectedItemId =
+                          cartItems[index].equipment.id;
+                      context.read<CartBloc>().add(
+                        CartSelectItem(selectedItemId: selectedItemId),
+                      );
+                    },
+                    onDelete: () {
+                      final selectedItemId =
+                          cartItems[index].equipment.id;
+                      context.read<CartBloc>().add(
+                        CartDeleteItem(selectedItemId: selectedItemId),
+                      );
+                    },
+                    onTap: () {},
+                  );
                 },
-                onDelete: () {
-                  print("$index: onDelete");
-                },
-                onTap: () {
-
-                },
+                separatorBuilder:
+                    (BuildContext context, int index) => Divider(
+                      color: Colors.black.withValues(alpha: 0.3),
+                      height: 12,
+                    ),
               );
             },
-            separatorBuilder: (BuildContext context, int index) => Divider(
-              color: Colors.black.withValues(alpha: 0.3),
-              height: 12,
-            ),
           ),
         ),
       ),
@@ -143,10 +147,7 @@ class _CartPageState extends State<CartPage> {
             decoration: BoxDecoration(
               color: Colors.white,
               border: Border(
-                top: BorderSide(
-                  color: Colors.grey.shade300,
-                  width: 1,
-                ),
+                top: BorderSide(color: Colors.grey.shade300, width: 1),
               ),
             ),
             padding: EdgeInsets.fromLTRB(16, 8, 16, bottomPadding),
@@ -167,7 +168,10 @@ class _CartPageState extends State<CartPage> {
                     ),
                     child: Text(
                       '대여하기',
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
                 ),
