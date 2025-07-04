2866f253eb3cdc73980311f290191132ef9e34d5
dw0124 <doowone8650@naver.com>
2025-07-04 22:12:25 +0900

    CartBloc - CartEvent (단일 선택, 단일 삭제, 전체 선택, 선택 삭제)구현

--- lib/cart/bloc/cart_bloc.dart
+++ lib/cart/bloc/cart_bloc.dart
@@ -1,37 +1,99 @@
 import 'package:bloc/bloc.dart';
 import 'package:induk/cart/bloc/cart_event.dart';
 import 'package:induk/cart/bloc/cart_state.dart';
+import 'package:induk/cart/model/cart_item.dart';
 import 'package:induk/cart/repository/cart_repository.dart';
+import 'package:induk/common/models/equipment.dart';
 
 class CartBloc extends Bloc<CartEvent, CartState> {
 
   final CartRepository _CartRepository;
 
-  CartBloc(this._CartRepository) : super(CartState(cartItems: [])) {
-    on<CartFetch>((event, emit) async {
+  CartBloc(this._CartRepository) : super(CartState(cartItems: [
+    CartItem(equipment: Equipment.empty, startDate: DateTime.now(), endDate: DateTime.now()),
+    CartItem(equipment: Equipment.empty2, startDate: DateTime.now(), endDate: DateTime.now()),
+    CartItem(equipment: Equipment.empty3, startDate: DateTime.now(), endDate: DateTime.now()),
+    CartItem(equipment: Equipment.empty4, startDate: DateTime.now(), endDate: DateTime.now()),
+    CartItem(equipment: Equipment.empty5, startDate: DateTime.now(), endDate: DateTime.now()),
+    CartItem(equipment: Equipment.empty6, startDate: DateTime.now(), endDate: DateTime.now()),
+    CartItem(equipment: Equipment.empty7, startDate: DateTime.now(), endDate: DateTime.now()),
+    CartItem(equipment: Equipment.empty8, startDate: DateTime.now(), endDate: DateTime.now()),
+  ])) {
+    on<CartFetch>((event, emit) async => ());
 
-    });
+    on<CartSelectAll>((event, emit) async => await _selectAll(event, emit));
 
-    on<CartSelectAll>((event, emit) async {
+    on<CartDeleteSelectedItems>((event, emit) async => _deleteSelectedItems(event, emit));
 
-    });
+    on<CartSelectItem>((event, emit) async => await _selectItem(event, emit));
 
-    on<CartDeleteSelectedItems>((event, emit) async {
+    on<CartDeleteItem>((event, emit) async => await _deleteItem(event, emit));
 
-    });
+    on<CartRentSelectedRequested>((event, emit) => ());
+  }
 
-    on<CartSelectItem>((event, emit) async {
+  Future<void> _selectAll(CartSelectAll event, Emitter<CartState> emit) async {
+    final isSelected = event.isSelected;
 
-    });
+    final selectedItemsId = Set<int>.from(state.selectedItemsId);
 
-    on<CartDeleteItem>((event, emit) async {
+    if (isSelected) {
+      state.cartItems.forEach((cartItem) {
+        selectedItemsId.add(cartItem.equipment.id);
+      });
+    } else {
+      selectedItemsId.clear();
+    }
 
-    });
+    final newState = state.copyWith(selectedItemsId: selectedItemsId);
 
-    on<CartRentSelectedRequested>((event, emit) async {
+    emit(newState);
+  }
 
-    });
+  Future<void> _deleteSelectedItems(CartDeleteSelectedItems event, Emitter<CartState> emit) async {
+    final selectedItemsId = state.selectedItemsId;
 
+    final updatedCartItems = state.cartItems
+        .where((item) => !selectedItemsId.contains(item.equipment.id))
+        .toList();
 
+    emit(state.copyWith(
+      cartItems: updatedCartItems,
+      selectedItemsId: {},
+    ));
+  }
+
+  Future<void> _selectItem(CartSelectItem event, Emitter<CartState> emit) async {
+    final selectedItemId = event.selectedItemId;
+
+    final selectedItemsId = Set<int>.from(state.selectedItemsId);
+
+    if (selectedItemsId.contains(selectedItemId)) {
+      selectedItemsId.remove(selectedItemId);
+    } else {
+      selectedItemsId.add(selectedItemId);
+    }
+
+    final newState = state.copyWith(selectedItemsId: selectedItemsId);
+
+    emit(newState);
+  }
+
+  Future<void> _deleteItem(CartDeleteItem event, Emitter<CartState> emit) async {
+    final selectedItemId = event.selectedItemId;
+
+    final updatedSelectedItems = Set<int>.from(state.selectedItemsId);
+    updatedSelectedItems.remove(selectedItemId);
+
+    final updatedCartItems = state.cartItems
+        .where((item) => item.equipment.id != selectedItemId)
+        .toList();
+
+    final newState = state.copyWith(
+        cartItems: updatedCartItems,
+        selectedItemsId: updatedSelectedItems
+    );
+
+    emit(newState);
   }
 }
