6c2d04a25a10029fac84ccf6a4a8ac8fdb6fb343
dw0124 <doowone8650@naver.com>
2025-07-07 23:24:54 +0900

    CartRepository

--- lib/features/cart/repository/cart_repository.dart
+++ lib/features/cart/repository/cart_repository.dart
@@ -1,3 +1,33 @@
-class CartRepository {
+import 'package:induk/features/cart/provider/cart_api_provider.dart';
 
+class CartRepository {
+  CartRepository({CartApiProvider? cartApiProvider})
+      : _cartApiProvider = cartApiProvider ?? CartApiProvider();
+
+  final CartApiProvider _cartApiProvider;
+
+  void fetchCartItems() {
+    _cartApiProvider.fetchCartItems();
+  }
+
+  void rentCartItems({
+    required List<int> cartItemIds,
+    required DateTime startAt,
+    required DateTime endAt,
+  }) {
+    final startAtString = startAt.toIso8601String();
+    final endAtString = endAt.toIso8601String();
+
+    _cartApiProvider.rentCartItems(cartItemIds: cartItemIds, startAt: startAtString, endAt: endAtString);
+  }
+
+  void deleteCartItems({
+    required List<int> cartItemIds,
+  }) async {
+    final result = await _cartApiProvider.deleteCartItems(cartItemIds: cartItemIds);
+  }
+
+  void dispose() {
+    _cartApiProvider.close();
+  }
 }
