b3ed829b34efe000b1b6c08f519b513834c25f93
dw0124 <doowone8650@naver.com>
2025-07-09 14:44:05 +0900

    CartItem - 프로퍼티 및 생성자 추가

--- lib/features/cart/model/cart_item.dart
+++ lib/features/cart/model/cart_item.dart
@@ -1,9 +1,39 @@
 import 'package:induk/common/models/equipment.dart';
 
 class CartItem {
+  final int id;
+  final String? startRentAt;
+  final String? endRentAt;
+  final String? renterName;
+  final String status;
+  final String createdAt;
   final Equipment equipment;
 
   CartItem({
+    required this.id,
+    this.startRentAt,
+    this.endRentAt,
+    this.renterName,
+    required this.status,
+    required this.createdAt,
     required this.equipment,
   });
+
+  factory CartItem.fromJson(Map<String, dynamic> json) {
+    final id = json['id'];
+    final startRentAt = json['startRentAt'];
+    final endRentAt = json['endRentAt'];
+    final status = json['status'];
+    final createdAt = json['createdAt'];
+    final equipment = Equipment.fromJson(json['model']);
+
+    return CartItem(
+        id: id,
+        startRentAt: startRentAt,
+        endRentAt: endRentAt,
+        status: status,
+        createdAt: createdAt,
+        equipment: equipment
+    );
+  }
 }
