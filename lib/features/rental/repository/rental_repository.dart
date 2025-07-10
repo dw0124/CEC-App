228722c8f7d2d361a745c8d70592c15e1d03d828
dw0124 <doowone8650@naver.com>
2025-07-10 21:20:09 +0900

    fetchEquipmentModels 추가

--- lib/features/rental/repository/rental_repository.dart
+++ lib/features/rental/repository/rental_repository.dart
@@ -1,4 +1,6 @@
+import 'package:induk/common/models/equipment.dart';
 import 'package:induk/common/models/equipment_category.dart';
+import 'package:induk/features/rental/model/equipment_response.dart';
 import 'package:induk/features/rental/provider/rental_api_provider.dart';
 
 class RentalRepository {
@@ -12,6 +14,25 @@ class RentalRepository {
 
 
     return List.empty();
+  }
+
+  Future<List<Equipment>> fetchEquipmentModels({
+    required int page,
+    required String categoryId,
+    String? searchKeyword,
+    String sortDirection = "ASC"
+  }) async {
+    final equipmentModelsJson = await _rentalApiProvider.fetchEquipmentModels(
+      page: 0,
+      categoryId: categoryId,
+      searchKeyword: searchKeyword,
+      sortDirection: sortDirection,
+    );
+
+    final equipmentResponse = EquipmentResponse.fromJson(equipmentModelsJson);
+    final equipmentList = equipmentResponse.equipmentList;
+
+    return equipmentList;
   }
 
   void dispose() {
