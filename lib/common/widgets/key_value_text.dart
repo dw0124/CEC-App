e188af0ce2385cf3c3b56d25fbff51c317ce5178
dw0124 <doowone8650@naver.com>
2025-07-14 00:09:27 +0900

    SpacedLabel - keyStyle, valueStyle 추가

--- lib/common/widgets/key_value_text.dart
+++ lib/common/widgets/key_value_text.dart
@@ -4,14 +4,18 @@ import 'package:induk/common/widgets/spacedLabel.dart';
 class KeyValueText extends StatelessWidget {
   final double spacing;
   final String keyString;
+  final TextStyle? keyStyle;
   final String valueString;
+  final TextStyle? valueStyle;
   final double labelWidth;
 
   const KeyValueText({
     super.key,
     this.spacing = 8,
     required this.keyString,
+    this.keyStyle,
     required this.valueString,
+    this.valueStyle,
     this.labelWidth = 70,
   });
 
@@ -21,8 +25,11 @@ class KeyValueText extends StatelessWidget {
       spacing: spacing,
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        SpacedLabel(text: keyString, width: labelWidth),
-        Text(valueString),
+        SpacedLabel(text: keyString, width: labelWidth, style: keyStyle),
+        Text(
+            valueString,
+          style: valueStyle,
+        ),
       ],
     );
   }
