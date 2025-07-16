046ddd5f8d6383a8eca0c831882ffb8dd3d1b19c
dw0124 <doowone8650@naver.com>
2025-07-16 19:17:53 +0900

    addToCart 추가

--- lib/features/rental/rental_apply/bloc/rental_apply_bloc.dart
+++ lib/features/rental/rental_apply/bloc/rental_apply_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:induk/common/models/equipment.dart';
+import 'package:induk/common/models/result.dart';
 import 'package:induk/features/rental/rental_apply/bloc/rental_apply_event.dart';
 import 'package:induk/features/rental/rental_apply/bloc/rental_apply_state.dart';
 import 'package:induk/features/rental/repository/rental_repository.dart';
@@ -10,5 +11,21 @@ class RentalApplyBloc extends Bloc<RentalApplyEvent, RentalApplyState> {
 
   RentalApplyBloc(this._rentalRepository, Equipment equipment) : super(RentalApplyState.initial(equipment: equipment)) {
     //on<RentalApplyFetch>((event, emit) async => await () {});
+
+    on<RentalApplyAddToCart>((event, emit) async => await addToCart(event, emit));
+  }
+
+  Future<void> addToCart(RentalApplyAddToCart event, Emitter<RentalApplyState> emit) async {
+    final id = state.equipment.id;
+    final result = await _rentalRepository.requestAddToCart(id: id);
+
+    switch (result) {
+      case Success(:final value):
+        emit(state.copyWith(addCartResult: result));
+        break;
+      case Failure(:final error):
+        emit(state.copyWith(addCartResult: Failure(error)));
+        break;
+    }
   }
 }
