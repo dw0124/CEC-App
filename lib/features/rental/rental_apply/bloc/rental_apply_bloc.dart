f340ed50367bd527faf58bd298b33b22286da62b
dw0124 <doowone8650@naver.com>
2025-07-17 17:08:32 +0900

    RentalApplyBloc - RentalApplyRentRequested, RentalApplyDateTimeUpdated 추가

--- lib/features/rental/rental_apply/bloc/rental_apply_bloc.dart
+++ lib/features/rental/rental_apply/bloc/rental_apply_bloc.dart
@@ -10,9 +10,9 @@ class RentalApplyBloc extends Bloc<RentalApplyEvent, RentalApplyState> {
   final RentalRepository _rentalRepository;
 
   RentalApplyBloc(this._rentalRepository, Equipment equipment) : super(RentalApplyState.initial(equipment: equipment)) {
-    //on<RentalApplyFetch>((event, emit) async => await () {});
-
     on<RentalApplyAddToCart>((event, emit) async => await addToCart(event, emit));
+    on<RentalApplyRentRequested>((event, emit) async => await _rentItem(event, emit));
+    on<RentalApplyDateTimeUpdated>((event, emit) async => await _dateTimeUpdated(event, emit));
   }
 
   Future<void> addToCart(RentalApplyAddToCart event, Emitter<RentalApplyState> emit) async {
@@ -26,6 +26,35 @@ class RentalApplyBloc extends Bloc<RentalApplyEvent, RentalApplyState> {
       case Failure(:final error):
         emit(state.copyWith(addCartResult: Failure(error)));
         break;
+    }
+  }
+
+  Future<void> _rentItem(RentalApplyRentRequested event, Emitter<RentalApplyState> emit) async {
+    final rentItemId = state.equipment.id;
+    final startAt = state.startAt;
+    final endAt = state.endAt;
+
+    _rentalRepository.rentCartItems(cartItemIds: [rentItemId], startAt: startAt, endAt: endAt);
+  }
+
+  Future<void> _dateTimeUpdated(RentalApplyDateTimeUpdated event, Emitter<RentalApplyState> emit) async {
+    final update = event.isStart ? state.startAt : state.endAt;
+
+    final date = event.date;
+    final time = event.time;
+
+    final updated = DateTime(
+      date?.year ?? update.year,
+      date?.month ?? update.month,
+      date?.day ?? update.day,
+      time?.hour ?? update.hour,
+      time?.minute ?? update.minute,
+    );
+
+    if (event.isStart) {
+      emit(state.copyWith(startAt: updated));
+    } else {
+      emit(state.copyWith(endAt: updated));
     }
   }
 }
