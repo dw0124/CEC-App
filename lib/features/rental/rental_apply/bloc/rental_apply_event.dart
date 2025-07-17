75534b4dcb7b5aa4c0e32ba26c13fbecebe596b2
dw0124 <doowone8650@naver.com>
2025-07-17 17:08:10 +0900

    RentalApplyEvent - RentalApplyRentRequested, RentalApplyDateTimeUpdated 추가

--- lib/features/rental/rental_apply/bloc/rental_apply_event.dart
+++ lib/features/rental/rental_apply/bloc/rental_apply_event.dart
@@ -1,5 +1,23 @@
+import 'package:flutter/material.dart';
+
 sealed class RentalApplyEvent {}
 
 final class RentalApplyFetch extends RentalApplyEvent {}
 
 final class RentalApplyAddToCart extends RentalApplyEvent {}
+
+/// 장비 대여하기 이벤트
+final class RentalApplyRentRequested extends RentalApplyEvent {}
+
+/// 대여시간 입력 받는 이벤트
+class RentalApplyDateTimeUpdated extends RentalApplyEvent {
+  final DateTime? date;
+  final TimeOfDay? time;
+  final bool isStart; // true면 startAt, false면 endAt
+
+  RentalApplyDateTimeUpdated({
+    this.date,
+    this.time,
+    required this.isStart,
+  });
+}
