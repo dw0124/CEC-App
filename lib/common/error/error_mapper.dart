b8bae3a249c93715cf7522aac9274a6c06b8ca92
dw0124 <doowone8650@naver.com>
2025-07-22 14:51:38 +0900

    ErrorMapper - toFailure 수정

--- lib/common/error/error_mapper.dart
+++ lib/common/error/error_mapper.dart
@@ -5,11 +5,12 @@ import 'package:induk/common/models/result.dart';
 
 class ErrorMapper {
   static Result<T> toFailure<T>(Object error) {
-    Exception? originalException = error is Exception ? error : null;
+    Exception originalException;
     String? userFriendlyMessage;
 
     // 1. TokenException (사용자 정의 인증 에러) 처리
     if (error is TokenException) {
+      originalException = error;
       switch (error.type) {
         case TokenErrorType.notFound:
           userFriendlyMessage = '로그인 정보가 없습니다. 다시 로그인해 주세요.';
@@ -24,23 +25,33 @@ class ErrorMapper {
     }
     // 2. http.ClientException 처리 (네트워크 오류, 타임아웃 등)
     else if (error is HttpException) {
+      originalException = error;
       userFriendlyMessage = error.message;
-    } else if (error is http.ClientException) { // http 패키지의 ClientException (네트워크 연결 문제 등)
+    } else if (error is http.ClientException) {
+      originalException = error;
       userFriendlyMessage = '네트워크 연결이 불안정합니다. 인터넷 연결을 확인하고 다시 시도해주세요.';
     }
-
     // 3. Dart의 내장 Exception 처리
     else if (error is FormatException) {
+      originalException = error;
       userFriendlyMessage = '데이터 형식이 올바르지 않습니다. 다시 시도해주세요.';
     } else if (error is TimeoutException) {
-      // http.ClientException에 포함되지 않는 다른 비동기 작업의 타임아웃
+      originalException = error;
       userFriendlyMessage = '작업 시간이 초과되었습니다. 다시 시도해주세요.';
     } else if (error is ArgumentError) {
+      originalException = Exception('ArgumentError: ${error.message}');
       userFriendlyMessage = '잘못된 입력값입니다.';
     }
+    // 5. 그 외 알 수 없는 모든 에러 처리 (가장 일반적인 Exception으로 처리)
+    else if (error is Exception) {
+      originalException = error;
+      userFriendlyMessage = '알 수 없는 오류가 발생했습니다: ${error.toString()}'; // 좀 더 상세하게
+    }
+    else { // error가 Exception 타입이 아닌 경우 (예: Error, String 등)
+      originalException = Exception('Unknown caught error of type ${error.runtimeType}');
+      userFriendlyMessage = '알 수 없는 오류가 발생했습니다.';
+    }
 
-    // 5. 그 외 알 수 없는 모든 에러 처리
-    return Failure(originalException ?? Exception('Unknown caught error'),
-        message: userFriendlyMessage ?? '알 수 없는 오류가 발생했습니다.');
+    return Failure(originalException, message: userFriendlyMessage ?? '알 수 없는 오류가 발생했습니다.');
   }
 }
