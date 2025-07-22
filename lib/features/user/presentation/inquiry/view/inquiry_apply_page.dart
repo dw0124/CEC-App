4f3aa9ae6170f1c14210cbb044a6ccfa48f73341
dw0124 <doowone8650@naver.com>
2025-07-22 20:52:33 +0900

    InquiryApplyPage - 문의글 작성 페이지

---
+++ lib/features/user/presentation/inquiry/view/inquiry_apply_page.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:induk/common/widgets/app_button.dart';
+import 'package:induk/common/widgets/deletable_image_tile.dart';
+import 'package:induk/features/user/model/inquiry.dart';
+
+
+class InquiryApplyPage extends StatefulWidget {
+  const InquiryApplyPage({super.key});
+
+  @override
+  State<InquiryApplyPage> createState() => _InquiryApplyPageState();
+}
+
+class _InquiryApplyPageState extends State<InquiryApplyPage> {
+
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
+
+  List<XFile> imagePathList = [];
+
+  InquiryType _selectedType = InquiryType.RENTAL;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            '1:1 문의',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+            child: Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      spacing: 16,
+                      children: [
+                        DropdownButtonFormField<InquiryType>(
+                          value: _selectedType,
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(horizontal: 12,
+                                vertical: 14),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Colors.grey, width: 1.0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Colors.grey, width: 1.0),
+                            ),
+                          ),
+                          onChanged: (InquiryType? newValue) {
+                            if (newValue != null) {
+                              setState(() {
+                                _selectedType = newValue;
+                              });
+                            }
+                          },
+                          items: InquiryType.values.map((InquiryType type) {
+                            return DropdownMenuItem<InquiryType>(
+                              value: type,
+                              child: Text(type.displayName),
+                            );
+                          }).toList(),
+                        ),
+
+
+                        // 제목
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('제목'),
+                            TextField(
+                              controller: _titleController,
+                              decoration: InputDecoration(
+                                border: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.grey, width: 1.0),
+                                  borderRadius: BorderRadius.all(
+                                      Radius.circular(5.0)),
+                                ),
+                                hintText: '제목을 입력해주세요',
+                                hintStyle: TextStyle(color: Colors.grey),
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 10.0, horizontal: 12.0),
+                                focusedBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.blue, width: 2.0),
+                                  borderRadius: BorderRadius.all(
+                                      Radius.circular(5.0)),
+                                ),
+                                enabledBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.grey, width: 1.0),
+                                  borderRadius: BorderRadius.all(
+                                      Radius.circular(5.0)),
+                                ),
+                              ),
+                              textAlignVertical: TextAlignVertical.center,
+                              maxLines: 1,
+                            ),
+                          ],
+                        ),
+
+                        // 내용
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('내용'),
+                            Container(
+                              width: double.infinity,
+                              height: 300,
+                              padding: const EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey),
+                                borderRadius: BorderRadius.circular(5.0),
+                              ),
+                              child: TextField(
+                                controller: _contentController,
+                                maxLines: null,
+                                expands: true,
+                                keyboardType: TextInputType.multiline,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: '내용을 입력해주세요...',
+                                ),
+                              ),
+                            ),
+                            // 또 다른 위젯들...
+                          ],
+                        ),
+
+                        // 사진
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('사진'),
+
+                            AppButton(
+                              text: '사진선택', onPressed: () async {
+                                final ImagePicker picker = ImagePicker();
+
+                                final List<XFile> images = await picker.pickMultiImage(limit: 3);
+
+                                setState(() {
+                                  imagePathList = [...imagePathList, ...images];
+                                });
+                              }
+                            ),
+
+                            Container(
+                              height: 120,
+                              width: double.infinity,
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: imagePathList.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  final imagePath = imagePathList[index].path;
+
+                                  return DeletableImageTile(
+                                      imagePath: imagePath,
+                                      onDelete: () {
+                                        setState(() {
+                                          imagePathList.removeAt(index);
+                                        });
+                                      }
+                                  );
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        SizedBox(height: 8,)
+                      ],
+                    ),
+                  ),
+                ),
+
+                Container(
+                  width: double.infinity,
+                  child: AppButton(
+                      text: '문의하기',
+                      onPressed: () {}
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
