cef89f920b3759d974e2cc7e176f3ef08cb2a0fc
dw0124 <doowone8650@naver.com>
2025-07-30 23:06:35 +0900

    스플래시 스크린 추가, 액세스 토큰 검사 후 로그인 or 메인 페이지 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 import 'package:induk/common/repository/token_repository.dart';
-import 'package:induk/login/login_page.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import 'package:induk/common/widgets/colors.dart';
+import 'package:induk/features/login/login_page.dart';
+
+import 'package:induk/features/notice/repository/notice_repository.dart';
+import 'package:induk/features/notice/bloc/notice_bloc.dart';
+import 'package:induk/features/notice/view/notice_page.dart';
+
+import 'package:induk/features/rental/repository/rental_repository.dart';
+import 'package:induk/features/rental/rental_main/bloc/rental_bloc.dart';
+import 'package:induk/features/rental/rental_main/view/rental_page.dart';
+
+import 'package:induk/features/user/presentation/user_main/view/user_page.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: 'assets/config/.env');
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
-  final accessToken = await TokenRepository().getAccessToken();
+  await dotenv.load(fileName: 'assets/config/.env');
 
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  late final Future<bool> _initializationFuture;
+
+  int _selectedIndex = 0;
+  late final List<Widget> _pages;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Future<bool> _initializeAppAndCheckLogin() async {
+    final accessToken = await TokenRepository().getAccessToken();
+    final isLoggedIn = accessToken.isNotEmpty;
+
+    await Future.delayed(Duration(seconds: 2));
+
+    FlutterNativeSplash.remove();
+
+    return isLoggedIn;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pages = [
+      RepositoryProvider(
+        create: (context) => RentalRepository(),
+        child: BlocProvider(
+          create: (context) => RentalBloc(context.read<RentalRepository>()),
+          child: const RentalPage(),
+        ),
+      ),
+      RepositoryProvider(
+        create: (context) => NoticeRepository(),
+        child: BlocProvider(
+          create: (context) => NoticeBloc(context.read<NoticeRepository>()),
+          child: const NoticePage(),
+        ),
+      ),
+      UserPage()
+    ];
+
+    _initializationFuture = _initializeAppAndCheckLogin();
+  }
+
+  @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: LoginPage(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0
+        ),
+      ),
+      home: FutureBuilder(
+          future: _initializationFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SizedBox.shrink();
+            } else {
+              if (snapshot.hasData && snapshot.data == true) {
+                return Scaffold(
+                  body: _pages.elementAt(_selectedIndex),
+                  bottomNavigationBar: BottomNavigationBar(
+                    items: const <BottomNavigationBarItem>[
+                      BottomNavigationBarItem(
+                          icon: Icon(Icons.home), label: '장비대여'),
+                      BottomNavigationBarItem(
+                          icon: Icon(Icons.business), label: '공지사항'),
+                      BottomNavigationBarItem(
+                          icon: Icon(Icons.account_circle_outlined),
+                          label: '마이페이지'),
+                    ],
+                    currentIndex: _selectedIndex,
+                    selectedItemColor: AppColors.main,
+                    onTap: _onItemTapped,
+                  ),
+                );
+              } else {
+                return LoginPage();
+              }
+            }
+          },
+      ),
     );
   }
 }
